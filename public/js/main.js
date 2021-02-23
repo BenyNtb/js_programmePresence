50346254ed813abee23ae2979c9c0ab7c76da1f5
Beny <b.ntambwe97@gmail.com>
2021-02-23 10:57:48 +0100

    IP address and date

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+let divStag = document.querySelector('#listStagiaires');
+let listStag = document.querySelector('.listStag');
+
+let inpKey = document.querySelector('#inpKey');
+let inpValue = document.querySelector('#inpValue');
+let ajouteStag = document.querySelector('.ajouteStag');
+
+let removeStag = document.querySelector('.removeStag');
+let refreshStag = document.querySelector('.refreshStag');
+
+
+// ajouter
+ajouteStag.onclick =  function() {
+    const key = inpKey.value;
+    const value = inpValue.value;
+
+    if (key && value) {
+        localStorage.setItem(key, value);
+        location.reload();
+    }
+}; 
+
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    // Ma liste
+    let list = document.createElement('li');
+    list.innerText = inpValue.value;
+    list.classList.add('stagiaire');
+    listStag.appendChild(list);
+
+    listStag.innerHTML += `${value}`;
+    // bouton de check
+    let checkBtn = document.createElement('button');
+    checkBtn.innerHTML = '<i class="fas fa-check"></i>';
+    checkBtn.classList.add('check-btn');
+    listStag.appendChild(checkBtn);
+    // bouton de supprimer
+    let removeBtn = document.createElement('button');
+    removeBtn.innerHTML = '<i class="fas fa-trash"></i> remove';
+    removeBtn.classList.add('remove-btn');
+    listStag.appendChild(removeBtn);
+};
+
+
+
+// IP ADDRESS/FOURNISSEUR
+
+fetch('http://api.ipify.org/?format=json')
+.then(res => res.json())
+.then(data => console.log(data))
+// console.log(`Votre adresse ip : ${data.json}`);
+
+
+
+// fetch('http://ip-api.com/json?callback=?')
+// .then(res => res())
+// .then(data => console.log(data))
+
+
+// DATE 
+
+let date = new Date();
+console.log(date);
+
+
+
+
+
